4cbecbbc5d86de1f5c7e6e075bcb8c9bdce40526
정지원 <stopone3119@gmail.com>
2024-03-23 17:27:19 +0900

    Create ContextExtensions.kt

---
+++ UiUtils/ContextExtensions.kt
@@ -0,0 +1,7 @@
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showToast(@StringRes resId: Int) {
+    Toast.makeText(this, getString(resId), Toast.LENGTH_SHORT).show()
+}
